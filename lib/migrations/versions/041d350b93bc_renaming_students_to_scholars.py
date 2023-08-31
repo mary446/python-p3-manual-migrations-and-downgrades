7041b5bac5fc191142cc4c4ab54f39df4505246c
mary446 <135317707+mary446@users.noreply.github.com>
2023-08-31 20:50:26 +0300

    initial commit

---
+++ lib/migrations/versions/041d350b93bc_renaming_students_to_scholars.py
@@ -0,0 +1,24 @@
+"""Renaming students to scholars
+
+Revision ID: 041d350b93bc
+Revises: 791279dd0760
+Create Date: 2023-08-31 20:32:05.324367
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '041d350b93bc'
+down_revision = '791279dd0760'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
